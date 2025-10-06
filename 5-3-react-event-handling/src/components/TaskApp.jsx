44e4e2c9d96a5ebbca3849197c77b089a961c481
AL MUBARK <s202272080@ccm.kfupm.edu.sa>
2025-10-06 08:36:08 +0300

    task1

--- 5-3-react-event-handling/src/components/TaskApp.jsx
+++ 5-3-react-event-handling/src/components/TaskApp.jsx
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 import TaskList from "./TaskList";
 
 export default function TaskApp() {
-  
+  // Add useState for text
+  const [text, setText] = useState("");
+
   const handleSubmit = () => {
    
   };
@@ -25,8 +27,8 @@ export default function TaskApp() {
           type="text"
           placeholder="Type a task..."
           className="input"
-          // TODO: value={text}
-          // TODO: onChange={(e) => setText(e.target.value)}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
           onKeyDown={(e) => {
             if (e.key === "Enter") handleSubmit();
           }}
@@ -35,7 +37,8 @@ export default function TaskApp() {
           Submit
         </button>
       </div>
-
+      <p>{text}</p>
+      
       {/*Render Task List and Enable Delete */}
       {/*Pass tasks and onDelete */}
       <TaskList /* tasks={tasks} onDelete={handleDelete} */ />
